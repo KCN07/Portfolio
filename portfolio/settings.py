cf6c2e7f74f7bc2f19a06dc54e2528a922b4f80d
KCN007 <www.kaniz98@gmail.com>
2020-07-17 00:24:46 +0600

    commited again 2nd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =  os.environ['secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
